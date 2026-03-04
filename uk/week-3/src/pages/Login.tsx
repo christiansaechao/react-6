f3e971215e63a6e7d7cfebc05c65e557e306fe7b
christiansaechao <62818511+christiansaechao@users.noreply.github.com>
2026-03-03 17:59:49 -0800

    us week 3 day 1

--- uk/week-3/src/pages/Login.tsx
+++ uk/week-3/src/pages/Login.tsx
@@ -1,53 +1,44 @@
 import { useState } from "react";
+import { redirect } from "react-router";
 
 /**
- * In local storage:
- * Username
- * Password
- * Validate username and password in form with the ones in the local storage
- *
+ * username: string;
+ * password: string;
+ * isLoggedIn: boolean;
  */
 
-interface User {
-  username: string;
-  password: string;
-}
+export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
-const Login = () => {
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
-  const [user, setUser] = useState<User>();
+  function validateUser() {
+    if (
+      username !== localStorage.getItem("username") ||
+      password !== localStorage.getItem("password")
+    ) {
+      setError("Check credentials");
+      return null;
+    }
 
-  const saveUser = () => {
-    const user = localStorage.getItem("user");
 
-    const savedUser = user ? JSON.parse(user) : null;
-    if (!saveUser) return console.log("no user");
-    setUser(savedUser);
-  };
+    return redirect("/home");
+  }
 
   return (
-    <div className="flex justify-center items-center">
-      <form
-        onSubmit={saveUser}
-        action="submit"
-        className="flex flex-col items-center"
-      >
-        <h1 className="text-[20px] mt-10">Login</h1>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          type="email"
-          className="mt-10 p-5"
-        />
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          className="mt-10 p-5"
-        />
-        <button className="mt-10">Login</button>
-      </form>
-    </div>
+    <>
+      {error && <p>{error}</p>}
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={validateUser}>Submit</button>
+    </>
   );
 };
-
-export default Login;
--- uk/week-3/src/pages/Signup.tsx
+++ uk/week-3/src/pages/Signup.tsx
@@ -1,60 +1,31 @@
 import { useState } from "react";
-import { setUser } from "../stores/slices/userSlice";
-import { useDispatch } from "react-redux";
+import { redirect } from "react-router";
 
-/**
- * Store in local storage:
- * Username
- * Password
- * Validate username and password in form with the ones in the local storage
- */
-
-/**
- * Homework
- * Create Protected Layout
- * Validate user is logged in
- * Logout Button - updates local storage and logs user out
- */
-
-export const Signup = () => {
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
-
-  const dispatch = useDispatch();
-
-  const saveInfo = () => {
-    const user = {
-      username,
-      password,
-    };
-
-    if (!username || !password || username.length <= 0) {
-      throw new Error("No username or password provided")
-    }
-
-    localStorage.setItem("user", JSON.stringify(user));
-    localStorage.setItem("loggedIn", JSON.stringify(true));
-    dispatch(setUser(user.username || ""));
-  };
+function SignUp() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
 
   return (
-    <div className="flex justify-center items-center">
-      <h2>CREATE AN ACCOUNT</h2>
-      <h3>Name</h3>
-      <input type="text" placeholder="Enter your name" />
-      <h3>Email</h3>
-      <input
-        onChange={(e) => setUsername(e.target.value)}
-        type="text"
-        placeholder="Enter your password"
-      />
-      <h3>Password</h3>
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        type="text"
-        placeholder="Enter your password"
-      />
-      <button onClick={saveInfo}>Sign up</button>
-    </div>
+    <>
+      <input value={user} type="text" onChange={(e) => setUser(e.target.value)}>
+        Username
+      </input>
+      <input value={pass} type="text" onChange={(e) => setPass(e.target.value)}>
+        Password
+      </input>
+
+      <button
+        onClick={() => {
+          localStorage.setItem("username", user);
+          localStorage.setItem("password", pass);
+          localStorage.setItem("isLoggedIn", "true")
+          return redirect("/home");
+        }}
+      >
+        Submit
+      </button>
+    </>
   );
-};
+}
+
+export default SignUp;
