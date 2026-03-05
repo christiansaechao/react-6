74ddfeea07434c6d5b937329cc6f325148464098
christiansaechao <62818511+christiansaechao@users.noreply.github.com>
2026-03-04 18:16:44 -0800

    week 3 day 2 korea

--- usa/week-3/src/App.tsx
+++ usa/week-3/src/App.tsx
@@ -1,13 +1,27 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+export const Signup = () => {
+  /**
+   * 1. Form
+   *  - input: username
+   *  - input: password
+   * 2. Grab the values from the inputs and store them in our component
+   *  - useState(s)
+   * 3. Submit Button
+   *  - store the username, password into local storage, isLoggedIn: "true"
+   * 
+   * HOMEWORK - Mar 4, 2026
+   * 1. make sure username, password is valid before creating user
+   * 2. protected route => wrap our dashboard
+   *  - validate the user isLoggedIn (flag in localStorage)
+   * 3. Login
+   *  - prepopulate the username, password in the form fields on component mount
+   *  - remember me []
+   * 4. Dashboard <div>Welcome, {username}</div>
+   *  - store the user in global state: zustand, redux, context api
+   *  - { username: chris, password: test, isLoggedIn: true } 
+   *  - revalidate the user => global state
+   */
 
-  return (
-    <>
-    </>
-  );
-}
-
-export default App;
-   
+  return <></>;
+};
